79be45c4426c8753c2c6a6967ac6b76287fb4073
Sebastian Ohl <s.ohl@ostfalia.de>
2025-02-20 11:48:10 +0100

    adding bufio

---
+++ pytest_board_client/analogbuf.py
@@ -0,0 +1,21 @@
+from pytest_board_client.blinka import BlinkaConnection
+
+
+class AnalogBuffer(BlinkaConnection):
+
+    def __init__(
+        self,
+        socket_path,
+    ):
+        super().__init__(socket_path)
+        self._buffer = []
+        self._samplerate = None
+
+    def deinit(self):
+        return super().deinit()
+
+    def __del__(self):
+        return super().__del__()
+
+    def senf_buffer_to(self, buf):
+        self._send_packet({"buffer": buf})
