6d6f6645eb9f6fae83ee1b1c345bdb829eda16f2
Sebastian Ohl <s.ohl@ostfalia.de>
2025-02-20 15:53:00 +0100

    fix st7789

--- pytest_board_client/st7789.py
+++ pytest_board_client/st7789.py
@@ -11,13 +11,14 @@ class ST7789(BlinkaConnection):
         TWO_A = 1
         TWO_B = 2
         TWO_C = 3
-        OTHER = 4
+        COLMOD = 4
+        OTHER = 5
 
     def __init__(self,
                  socket_path,
                  new_image_callback=lambda x: 1 == 1,
-                 width=280,
-                 height=240):
+                 height=280,
+                 width=240):
         super().__init__(socket_path)
         self._width = width
         self._height = height
@@ -37,44 +38,50 @@ class ST7789(BlinkaConnection):
             raw_data = base64.b64decode(d['data'])
             if self._state == ST7789.State.TWO_A:
                 self._state = ST7789.State.OTHER
-                (_, self._window[0][0], _,
-                 self._window[0][1]) = struct.unpack("BBBB", raw_data)
-            if self._state == ST7789.State.TWO_B:
+                (self._window[0][0],
+                 self._window[0][1]) = struct.unpack(">HH", raw_data)
+            elif self._state == ST7789.State.TWO_B:
                 self._state = ST7789.State.OTHER
                 (self._window[1][0],
                  self._window[1][1]) = struct.unpack(">HH", raw_data)
-            if self._state == ST7789.State.TWO_C:
+            elif self._state == ST7789.State.TWO_C:
                 self._state = ST7789.State.OTHER
                 self._to_image(raw_data)
+            elif self._state == ST7789.State.COLMOD:
+                if raw_data != b'\x55':
+                    assert False, "color coding (0x%02x) other than 65K of RGB, 16bit/pixel is not supported" % int(
+                        raw_data[0])
+                self._state = ST7789.State.OTHER
             elif raw_data == b'\x2a':
                 self._state = ST7789.State.TWO_A
             elif raw_data == b'\x2b':
                 self._state = ST7789.State.TWO_B
             elif raw_data == b'\x2c':
                 self._state = ST7789.State.TWO_C
+            elif raw_data == b'\x3a':
+                self._state = ST7789.State.COLMOD
             else:
+                print("WARN: unhandled st7789 command %02x" % int(raw_data[0]))
                 self._state = ST7789.State.OTHER
 
     def _to_image(self, data: bytes):
         counter = 0
+        pixel_per_row = self._window[0][1] - self._window[0][0] + 1
         for counter in range(0, len(data), 2):
             bb = data[counter:counter + 2]
-
             pixel = struct.unpack(">H", bb)[0]
-            R = ((pixel >> 13) & 0x1f) * (255 // 0x1f)
+            R = ((pixel >> 11) & 0x1f) * (255 // 0x1f)
             G = ((pixel >> 5) & 0x3f) * (255 // 0x3f)
-            B = ((pixel >> 1) & 0x1f) * (255 // 0x1f)
-            x = self._window[0][0] + (counter // 2) // (self._window[0][1] -
-                                                        self._window[0][0] + 1)
-            y = self._window[1][0] + (counter // 2) % (self._window[0][1] -
-                                                       self._window[0][0] + 1)
+            B = ((pixel >> 0) & 0x1f) * (255 // 0x1f)
+            x = self._window[0][0] + (counter // 2) % pixel_per_row
+            y = self._window[1][0] + (counter // 2) // pixel_per_row
             if x < self._width and y < self._height:
                 self._im.putpixel((x, self._height - y - 1), (R, G, B))
             counter += 1
 
         self._new_image_callback(self)
 
-    def image(self):
+    def image(self) -> PIL.Image:
         return self._im
 
     def set_callback(self, callback):
